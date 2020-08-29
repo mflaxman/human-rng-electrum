b7f251d1438a8b8cb3e80d1de1d43ea816d2fc9a
Michael Flaxman <mflaxman@gmail.com>
2020-08-28 19:58:05 -0500

    coldcard -> specter setup

--- electrum.py
+++ electrum.py
@@ -1,5 +1,5 @@
 from electrum.keystore import from_bip39_seed, bip39_is_checksum_valid, Wordlist
-import json
+import json, textwrap 
 
 def get_last_word(first_words):
     _, is_wordlist_valid = bip39_is_checksum_valid(first_words)
@@ -27,17 +27,19 @@ def calculate_seed_and_xpubs(first_words):
     return ({
         'last_word': last_word,
         'whole_seed': whole_seed,
-        'whole_seed_len': len(whole_seed.split()),
+        'whole_seed_word_count': len(whole_seed.split()),
     }, {
       'xfp': ks.get_root_fingerprint().upper(),  # uppercase to match others, unsure if that matters
       'p2wsh': ks.get_master_public_key(),
       'p2wsh_deriv': derivation_path,
     })
 
-last_word_dict, cc_export_dict = calculate_seed_and_xpubs(first_words=first_words)
-print(json.dumps(last_word_dict, indent=4))
-print(json.dumps(cc_export_dict, indent=4))
+last_word_dict, specter_dict = calculate_seed_and_xpubs(first_words=first_words)
+# print(json.dumps(specter_dict, indent=4))  ## Can be used for ccxp-*.json file, but relying on Specter for this instead
+print('\n', '*'*99, '\n')
+print('PRIVATE SECRET TO WRITE DOWN:', '\n')
+print(textwrap.indent(text=json.dumps(last_word_dict, indent=2), prefix='  '))
+result = '  [{}/{}]{}'.format(specter_dict['xfp'], specter_dict['p2wsh_deriv'][2:], specter_dict['p2wsh'])
 
-f_output = '/tmp/ccxp-%s.json' % cc_export_dict['xfp']
-with open(f_output, 'w') as f: f.write(json.dumps(cc_export_dict, indent=4))
-print("Saved to: %s" % f_output)
+print('\n', 'PUBLIC KEY INFO TO LOAD INTO SPECTER-DESKTOP:', '\n')
+print(result, '\n')
