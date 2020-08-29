d913e83cf6bd3a31c057d9cee2652a7125dbadcb
Michael Flaxman <mflaxman@gmail.com>
2020-08-28 20:00:04 -0500

    blackify and cleanup

--- electrum.py
+++ electrum.py
@@ -1,5 +1,6 @@
 from electrum.keystore import from_bip39_seed, bip39_is_checksum_valid, Wordlist
-import json, textwrap 
+import json, textwrap
+
 
 def get_last_word(first_words):
     _, is_wordlist_valid = bip39_is_checksum_valid(first_words)
@@ -15,31 +16,39 @@ def get_last_word(first_words):
 
     raise Exception("Can't find valid checksum word")
 
+
 def calculate_seed_and_xpubs(first_words):
     last_word = get_last_word(first_words=first_words)
     whole_seed = first_words + " " + last_word
     is_checksum_valid, _ = bip39_is_checksum_valid(whole_seed)
     assert is_checksum_valid is True
 
-    derivation_path="m/48'/0'/0'/2'"
+    derivation_path = "m/48'/0'/0'/2'"
     ks = from_bip39_seed(seed=whole_seed, passphrase="", derivation=derivation_path)
 
-    return ({
-        'last_word': last_word,
-        'whole_seed': whole_seed,
-        'whole_seed_word_count': len(whole_seed.split()),
-    }, {
-      'xfp': ks.get_root_fingerprint().upper(),  # uppercase to match others, unsure if that matters
-      'p2wsh': ks.get_master_public_key(),
-      'p2wsh_deriv': derivation_path,
-    })
+    return (
+        {
+            "last_word": last_word,
+            "whole_seed": whole_seed,
+            "whole_seed_word_count": len(whole_seed.split()),
+        },
+        {
+            "xfp": ks.get_root_fingerprint().upper(),  # uppercase to match others, unsure if that matters
+            "p2wsh": ks.get_master_public_key(),
+            "p2wsh_deriv": derivation_path,
+        },
+    )
+
 
 last_word_dict, specter_dict = calculate_seed_and_xpubs(first_words=first_words)
-# print(json.dumps(specter_dict, indent=4))  ## Can be used for ccxp-*.json file, but relying on Specter for this instead
-print('\n', '*'*99, '\n')
-print('PRIVATE SECRET TO WRITE DOWN:', '\n')
-print(textwrap.indent(text=json.dumps(last_word_dict, indent=2), prefix='  '))
-result = '  [{}/{}]{}'.format(specter_dict['xfp'], specter_dict['p2wsh_deriv'][2:], specter_dict['p2wsh'])
+result = "  [{}/{}]{}".format(
+    specter_dict["xfp"], specter_dict["p2wsh_deriv"][2:], specter_dict["p2wsh"]
+)
 
-print('\n', 'PUBLIC KEY INFO TO LOAD INTO SPECTER-DESKTOP:', '\n')
-print(result, '\n')
+# print(json.dumps(specter_dict, indent=4))  ## Can be used for ccxp-*.json file, but relying on Specter for this instead
+print("\n", "*" * 99, "\n")
+print("PRIVATE SECRET TO WRITE DOWN:", "\n")
+print(textwrap.indent(text=json.dumps(last_word_dict, indent=2), prefix="  "))
+
+print("\n", "PUBLIC KEY INFO TO LOAD INTO SPECTER-DESKTOP:", "\n")
+print(result, "\n")
